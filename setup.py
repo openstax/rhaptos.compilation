34921d3a1e630c5f7ca75e9a47d85e933e4a5837
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-19 11:55:05 +0200

    First checkin of scaffolding.

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='rhaptos.compilation',
+      version=version,
+      description="Rhaptos Compilation",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Rijk Stofberg',
+      author_email='rijk@upfrontsystems.co.za',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['rhaptos'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.namedfile [blobs]',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      # The next two lines may be deleted after you no longer need
+      # addcontent support from paster and before you distribute
+      # your package.
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
