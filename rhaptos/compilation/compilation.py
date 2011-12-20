6cc610280b773bcc95a8421fb81074d941c2e19e
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-20 07:49:07 +0200

    Table of contents browser view.

--- rhaptos/compilation/compilation.py
+++ rhaptos/compilation/compilation.py
@@ -56,8 +56,46 @@ class View(grok.View):
     
     # grok.name('view')
 
+class TableOfContentsHelpers(grok.View):
+    """ Helper methods and a template that renders only the table of contents.
+    """
+    grok.context(ICompilation)
+    grok.require('zope2.View')
+    grok.name('rhaptos.compilation.table-of-contents-helpers')
+    
+    def getContentsAsDict(self):
+        compilations = self.getCompilations()
+        contentreferences = self.getContentReferences()
+        content = {'children': compilations,
+                   'contentreferences': contentreferences}
+        return content
+
+    def getCompilations(self, container=None):
+        contentFilter = {'object_provides':
+                         'rhaptos.compilation.compilation.ICompilation'}
+        container = container or self.context
+        brains = container.getFolderContents(contentFilter)
+        return [b.getObject() for b in brains] or []
+
+    def getContentReferences(self, container=None):
+        contentFilter = {'object_provides':
+                         'rhaptos.compilation.contentreference.IContentReference'}
+        container = container or self.context
+        brains = container.getFolderContents(contentFilter)
+        return [b.getObject() for b in brains] or []
+    
+    def isroot(self, context=None):
+        """ If the parent of 'context' does not provide ICompilation we can be
+            sure 'context' is the topmost (root) Compilation object.
+        """
+        context = context or self.context
+        parent = context.aq_parent
+        return not ICompilation.providedBy(parent)
+
 class TableOfContentsView(grok.View):
+    """ Renders only one level of compilations and contentreferences.
+    """
     grok.context(ICompilation)
     grok.require('zope2.View')
     grok.name('rhaptos.compilation.table-of-contents')
-
+    
