9ee440dc2bf754384a3621ef5f60f3eb60c5c523
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-21 10:34:59 +0200

    First checkin

---
+++ rhaptos/compilation/section.py
@@ -0,0 +1,56 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from rhaptos.compilation import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class ISection(form.Schema, IImageScaleTraversable):
+    """
+    Compilation section.
+    """
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+
+class Section(dexterity.Container):
+    grok.implements(ISection)
+    
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# section_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class View(grok.View):
+    grok.context(ISection)
+    grok.require('zope2.View')
+    
+    # grok.name('view')
