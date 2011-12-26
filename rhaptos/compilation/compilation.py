2d7c750e5f3767ee337f9d0bee53348db20f4df0
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-26 17:12:58 +0200

    Remove unused methods

--- rhaptos/compilation/compilation.py
+++ rhaptos/compilation/compilation.py
@@ -28,35 +28,6 @@ class TableOfContentsHelpers(grok.View):
     grok.require('zope2.View')
     grok.name('rhaptos.compilation.table-of-contents-helpers')
     
-    def getContentsAsDict(self):
-        compilations = self.getCompilations()
-        contentreferences = self.getContentReferences()
-        content = {'children': compilations,
-                   'contentreferences': contentreferences}
-        return content
-
-    def getCompilations(self, container=None):
-        contentFilter = {'portal_type':
-                         'rhaptos.compilation.compilation'}
-        container = container or self.context
-        brains = container.getFolderContents(contentFilter)
-        return [b.getObject() for b in brains] or []
-
-    def getContentReferences(self, container=None):
-        contentFilter = {'portal_type':
-                         'rhaptos.compilation.contentreference'}
-        container = container or self.context
-        brains = container.getFolderContents(contentFilter)
-        return [b.getObject() for b in brains] or []
-    
-    def isroot(self, context=None):
-        """ If the parent of 'context' does not provide ICompilation we can be
-            sure 'context' is the topmost (root) Compilation object.
-        """
-        context = context or self.context
-        parent = context.aq_parent
-        return not ICompilation.providedBy(parent)
-
     def getContentItems(self, container=None):
         container = container or self.context
         return container.getFolderContents(full_objects=True)
