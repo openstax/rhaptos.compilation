366a0255a78959c6ed48931c8c4ef6128cd99b95
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-26 17:14:07 +0200

    Sorting out my unit tests

--- rhaptos/compilation/tests/test_contentreference.py
+++ rhaptos/compilation/tests/test_contentreference.py
@@ -8,44 +8,19 @@ from plone.app.testing import TEST_USER_ID
 from Products.CMFCore.utils import getToolByName
 
 from rhaptos.compilation.contentreference import CompilationSourceBinder
+from rhaptos.compilation.compilation import TableOfContentsHelpers, ICompilation
+from rhaptos.compilation.section import ISection 
 
 from base import PROJECTNAME
 from base import INTEGRATION_TESTING
 
 dirname = os.path.dirname(__file__)
 
-class TestCompilation(unittest.TestCase):
-    """ Test marshal module """
+class TestCompilationSourceBinder(unittest.TestCase):
+    """ Test compilation source binder """
     layer = INTEGRATION_TESTING
 
-    def setUp(self):
-        self.portal = self.layer['portal']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-
-    def test_compilationsourcebinder_nocontent(self):
-        sourcebinder = CompilationSourceBinder(
-            object_provides='Products.CMFCore.interfaces._content.IContentish'
-        )
-        self.assertTrue(len(self.portal.portal_catalog())==0)
-        results = sourcebinder(self.portal).search('')
-        self.assertTrue(len(results)==0)
-    
-    def test_compilationsourcebinder_nocompilationcontent(self):
-        sourcebinder = CompilationSourceBinder(
-            object_provides='Products.CMFCore.interfaces._content.IContentish'
-        )
-        self.portal.invokeFactory('Document', 'd1', title=u"Document 1")
-        self.portal.invokeFactory('rhaptos.compilation.compilation',
-            'c1', title=u"Compilation 1")
-
-        # make sure both objects are catalogued
-        self.assertTrue(len(self.portal.portal_catalog())==2)
-
-        # compilation must be exclude from the source binder results
-        results = sourcebinder(self.portal).search('')
-        self.assertTrue(len(results)==1)
-    
-    def _createContent(self):
+    def _createSections(self):
         id = self.portal.invokeFactory(
             'rhaptos.compilation.compilation',
             'compilation001',
@@ -57,24 +32,12 @@ class TestCompilation(unittest.TestCase):
                     )
         for itemId, itemType, itemTitle in structure:
             compilation.invokeFactory(itemType, itemId, title=itemTitle)
-        
-    def test_compilationsourcebinder_withsections(self):
-        self._createContent()
-        # we should have 4 objects in the catalog
-        self.assertTrue(len(self.portal.portal_catalog())==4)
-
-        sourcebinder = CompilationSourceBinder(
-            object_provides='Products.CMFCore.interfaces._content.IContentish'
-        )
-        results = sourcebinder(self.portal).search('')
-        self.assertTrue(len(results)==0)
-
-    def test_compilationsourcebinder_withcontentrefs(self):
-        self._createContent()
-        compilation = self.portal._getOb('compilation001')
         sections = compilation.getFolderContents(
             {'portal_type': 'rhaptos.compilation.section'},
             full_objects=True)
+        return sections
+        
+    def _createContentRefs(self, sections):
         for number, section in enumerate(sections):
             id = self.portal.invokeFactory(
                 'File',
@@ -85,12 +48,44 @@ class TestCompilation(unittest.TestCase):
                 'contentref%s' %number,
                 title=u"Content reference %s" %number,
                 relatedContent = self.portal._getOb(id))
-        # we should have 10 objects in the catalog
-        # 1 compilation, 3 sections, 3 contentrefs, 3 files
-        self.assertTrue(len(self.portal.portal_catalog())==10)
 
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        sections = self._createSections()
+        self._createContentRefs(sections)
+        self.portal.invokeFactory('Document', 'd1', title=u"Document 1")
+
+    def test_compilationsourcebinder(self):
         sourcebinder = CompilationSourceBinder(
             object_provides='Products.CMFCore.interfaces._content.IContentish'
         )
+
+        # make sure all our objects are catalogued
+        self.assertTrue(len(self.portal.portal_catalog())==11)
+
+        # we should only get the one document and 3 files we create in setup
         results = sourcebinder(self.portal).search('')
-        self.assertTrue(len(results)==3)
+        self.assertTrue(len(results)==4)
+
+
+class TestTOCHelpers(TestCompilationSourceBinder):
+    """ Test compilation TOC helpers """
+    layer = INTEGRATION_TESTING
+
+    def test_getContentItems(self):
+        compilation = self.portal._getOb('compilation001')
+        tochelper = TableOfContentsHelpers(compilation, {}) 
+        items = tochelper.getContentItems()
+        self.assertTrue(len(items)==3)
+
+    def test_isCompilation(self):
+        compilation = self.portal._getOb('compilation001')
+        self.assertTrue(ICompilation.providedBy(compilation))
+
+    def test_isSection(self):
+        compilation = self.portal._getOb('compilation001')
+        tochelper = TableOfContentsHelpers(compilation, {}) 
+        items = tochelper.getContentItems()
+        for item in items:
+            self.assertTrue(ISection.providedBy(item))
